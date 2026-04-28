e55574ed3aa2bfec27a834dc80993ac05bfb9a42
Rijv99x <rjprinc.3o@gmail.com>
2026-04-28 16:22:04 +0800

    Update loader.js

--- api/loader.js
+++ api/loader.js
@@ -1,27 +1,21 @@
 export default function handler(req, res) {
-  const startTime = performance.now();
+  const userAgent = req.headers['user-agent'] || '';
 
-  try {
-    const userAgent = req.headers['user-agent'] || '';
+  const isRoblox =
+    userAgent.includes("Roblox") ||
+    req.headers['roblox-id'];
 
-    const isRoblox =
-      userAgent.includes("Roblox") ||
-      req.headers['roblox-id'];
-
-    if (!isRoblox) {
-      return res.status(403).send("nice try, better luck next time");
-    }
-
-    const endTime = performance.now();
-    const timeTaken = (endTime - startTime).toFixed(2);
-    const timingWarning = `warn("kynx.net")`;
-    const luaScript = `print("nice work")`;
-    const finalScript = timingWarning;
-
-    res.setHeader("Content-Type", "text/plain");
-    return res.status(200).send(finalScript);
-
-  } catch (error) {
-    return res.status(500).send("Internal Server Error");
+  if (!isRoblox) {
+    return res.status(403).send("nice try, better luck next time");
   }
+
+  const startTime = performance.now();
+  const endTime = performance.now();
+  const timeTaken = (endTime - startTime).toFixed(2);
+  const timingWarning = `warn("")`;
+  const luaScript = `loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-SECURE-DEX-AND-REMOTE-SPY-205256"))()`;
+  const finalScript = timingWarning;
+
+  res.setHeader("Content-Type", "text/plain");
+  return res.status(200).send(finalScript);
 }
