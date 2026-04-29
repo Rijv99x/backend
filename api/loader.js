42821f15545179a844505c58a096982d3dd7dbc7
Rijv99x <rjprinc.3o@gmail.com>
2026-04-29 14:16:02 +0800

    Refactor handler function for user agent check

--- api/loader.js
+++ api/loader.js
@@ -1,23 +1,18 @@
 export default function handler(req, res) {
-  try {
-    const userAgent = req.headers['user-agent'] || '';
+  const userAgent = req.headers['user-agent'] || '';
 
-    const isRoblox =
-      userAgent.includes("Roblox") ||
-      req.headers['roblox-id'];
+  const isRoblox =
+    userAgent.includes("Roblox") ||
+    req.headers['roblox-id'];
 
-    if (!isRoblox) {
-      const fs = require('fs');
-      const html = fs.readFileSync(process.cwd() + '/index.html', 'utf8');
-      res.setHeader("Content-Type", "text/html");
-      return res.status(200).send(html);
-    }
-
-    const luaScript = `loadstring(game:HttpGet("https://raw.githubusercontent.com/Fsploit/Akak/main/api/loader.txt"))()`;
-    res.setHeader("Content-Type", "text/plain");
-    return res.status(200).send(luaScript);
-
-  } catch (error) {
-    return res.status(500).send("Internal Server Error");
+  if (!isRoblox) {
+    const fs = require('fs');
+    const html = fs.readFileSync(process.cwd() + '/index.html', 'utf8');
+    res.setHeader("Content-Type", "text/html");
+    return res.status(200).send(html);
   }
+
+  const luaScript = `loadstring(game:HttpGet("https://raw.githubusercontent.com/Fsploit/Akak/main/api/loader.txt"))()`;
+  res.setHeader("Content-Type", "text/plain");
+  return res.status(200).send(luaScript);
 }
